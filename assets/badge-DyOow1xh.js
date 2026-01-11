9367b389dfcbe83dc5adc51207859743056990a9
Vidish Bijalwan <vidishbijalwan@gmail.com>
2026-01-11 19:13:51 +0530

    Updates

---
+++ assets/badge-DyOow1xh.js
@@ -0,0 +1,11 @@
+import{c as r,j as a,a as s,B as n}from"./index--xrktNsK.js";/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=r("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const p=r("Layers",[["path",{d:"m12.83 2.18a2 2 0 0 0-1.66 0L2.6 6.08a1 1 0 0 0 0 1.83l8.58 3.91a2 2 0 0 0 1.66 0l8.58-3.9a1 1 0 0 0 0-1.83Z",key:"8b97xw"}],["path",{d:"m22 17.65-9.17 4.16a2 2 0 0 1-1.66 0L2 17.65",key:"dd6zsq"}],["path",{d:"m22 12.65-9.17 4.16a2 2 0 0 1-1.66 0L2 12.65",key:"ep9fru"}]]),i=n("inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",{variants:{variant:{default:"border-transparent bg-primary text-primary-foreground hover:bg-primary/80",secondary:"border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",destructive:"border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",outline:"text-foreground",anomaly:"border-anomaly/30 bg-anomaly/20 text-anomaly",critical:"border-critical/30 bg-critical/20 text-critical",success:"border-success/30 bg-success/20 text-success",info:"border-info/30 bg-info/20 text-info",h0:"border-topo-h0/30 bg-topo-h0/20 text-topo-h0",h1:"border-topo-h1/30 bg-topo-h1/20 text-topo-h1",h2:"border-topo-h2/30 bg-topo-h2/20 text-topo-h2",pulse:"border-primary/30 bg-primary/20 text-primary animate-pulse",glow:"border-primary bg-primary/20 text-primary shadow-glow-primary"}},defaultVariants:{variant:"default"}});function b({className:t,variant:e,...o}){return a.jsx("div",{className:s(i({variant:e}),t),...o})}export{b as B,c as C,p as L};
